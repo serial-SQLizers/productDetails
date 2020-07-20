f33d728f840f5a23522b3ace23a9507db4a96a06
GuillaumeFitzrichard <alfredhwolfe@gmail.com>
2020-07-20 10:35:03 -0600

    local testing increased pool

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,9 @@
 const { Pool } = require('pg');
-
+// host: 'ec2-13-59-201-123.us-east-2.compute.amazonaws.com',
 const pool = new Pool({
-  host: 'ec2-13-59-201-123.us-east-2.compute.amazonaws.com',
+  host: '/var/run/postgresql',
   user: 'postgres',
+  max: 20,
   password: 'B4nO93V9mNF5',
   database: 'product_details',
 });
